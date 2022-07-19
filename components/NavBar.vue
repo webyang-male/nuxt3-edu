05264bda1249c3ddd02ed38022e1f68139605666
webyang-male <mr.yangweb@gmail.com>
2022-07-19 20:27:09 +0800

    :pencil: 头部导航菜单组件开发

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,10 @@
     <div class="navbar">
         <div class="container flex items-center h-[4rem]">
             <n-button text class="!ml-[3rem] !text-xl !font-bold ">追梦教育</n-button>
-            <div>导航区域</div>
+            <div class="menu">
+                <div class="menu-item menu-item-active">推荐课</div>
+                <div class="menu-item" v-for="i in 8" :key="i">分类{{i}}</div>
+            </div>
 
             <n-button circle class="ml-auto mr-[2rem]">
                 <template #icon>
@@ -23,7 +26,7 @@
 
 <script setup>
 //引入naive-ui组件
-import { NButton, NIcon, NDropdown,NAvatar } from 'naive-ui'
+import { NButton, NIcon, NDropdown, NAvatar } from 'naive-ui'
 import { Search } from '@vicons/ionicons5'
 
 const userOptions = [{
@@ -39,5 +42,15 @@ const userOptions = [{
 .navbar {
     z-index: 999;
     @apply bg-white fixed top-0 left-0 right-0 shadow-sm;
+}
+.menu{
+    @apply flex items-center px-4;
+}
+.menu-item{
+    transition: all .3s;
+    @apply px-2 py-1 mx-1 rounded hover:(bg-blue-50 text-blue-500) !active:(bg-blue-100);
+}
+.menu-item-active{
+    @apply !bg-blue-100 !text-blue-500;
 }
 </style>
