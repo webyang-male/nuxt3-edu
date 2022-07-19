5af8fad84db967cf4210d51f92343323d0f1af02
webyang-male <mr.yangweb@gmail.com>
2022-07-19 19:07:54 +0800

    :pencil: 引入xicons图标库和调用

--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,13 @@
         <nuxt-link to="/test">goto测试页</nuxt-link>
        <p>index</p> 
     </div>
+    <n-icon size="40">
+        <search />
+    </n-icon>
 </template>
+
+<script setup>
+//引入naive-ui图标
+import { NIcon } from 'naive-ui'
+import {Search } from '@vicons/ionicons5'
+</script>
