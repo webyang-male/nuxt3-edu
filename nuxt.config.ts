f6ea860f687121303a27a112fd751c712677e670
webyang-male <mr.yangweb@gmail.com>
2022-07-25 23:05:19 +0800

    页面标题描述和关键词配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,22 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: "%s - Nuxt3 | 大赵同学",
+      title: "nuxt3-ssr | 大赵同学",
+      charset: "utf-8",
+      htmlAttrs: {
+        lang: "zh-CN",
+      },
+      meta: [
+        { name: "keywords", content: "vue3,nuxt3,ssr,大赵同学,zain" },
+        { name: "description", content: "基于vue3的nuxt框架SSR教育demo站点" },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
-  buildModules: [
-    'nuxt-windicss',
-  ],
+  buildModules: ["nuxt-windicss"],
   build: {
     transpile:
       process.env.NODE_ENV === "production"
--- pages/index.vue
+++ pages/index.vue
@@ -4,43 +4,52 @@
             加载中...
         </template>
         <template v-else-if="error">
-           错误提示： {{ error?.data?.data }}
+            错误提示： {{ error?.data?.data }}
         </template>
         <template v-else>
-            <template v-for="(item,index) in data" :key="index">
-                <Banner :data="item.data" v-if="item.type == 'swiper'"/>
-                <ImageNav :data="item.data"  v-else-if="item.type == 'icons'"/>
-                <ImageAd :data="item.data" v-else-if="item.type == 'imageAd'"/>
-                <ListCard :data="item.data" :title="item.title" v-else-if="item.type == 'list'"/>
-                <ListCard :data="item.data" :type="item.listType" :title="item.title" v-else-if="item.type == 'promotion'"/>
+            <template v-for="(item, index) in data" :key="index">
+                <Banner :data="item.data" v-if="item.type == 'swiper'" />
+                <ImageNav :data="item.data" v-else-if="item.type == 'icons'" />
+                <ImageAd :data="item.data" v-else-if="item.type == 'imageAd'" />
+                <ListCard :data="item.data" :title="item.title" v-else-if="item.type == 'list'" />
+                <ListCard :data="item.data" :type="item.listType" :title="item.title"
+                    v-else-if="item.type == 'promotion'" />
             </template>
         </template>
-        
+
     </div>
 </template>
 <script setup>
-const { 
+//设置首页标题,举个例子，这里的title就是首页的标题
+useHead({
+    title: '首页',
+    meta: [
+        { name: "keywords", content: "vue3,nuxt3,ssr,大赵同学,zain" },
+        { name: "description", content: "基于vue3的nuxt框架SSR教育站点首页" },
+    ],
+})
+const {
     pending,
     data,
     error
-} = await useFetch("/index",{
-    key:"IndexData",
-    baseURL:"http://demonuxtapi.dishait.cn/pc",
-    headers:{
-        appid:"bd9d01ecc75dbbaaefce"
+} = await useFetch("/index", {
+    key: "IndexData",
+    baseURL: "http://demonuxtapi.dishait.cn/pc",
+    headers: {
+        appid: "bd9d01ecc75dbbaaefce"
     },
     // 响应之前数据处理
-    transform:(res)=>{
+    transform: (res) => {
         return res.data
     },
     // 是否开启缓存
-    initialCache:false,
+    initialCache: false,
     // 懒加载
-    lazy:true
+    lazy: true
 })
 
 // 服务端时直接报错
-if(process.server && error.value){
+if (process.server && error.value) {
     throwError(error.value?.data?.data)
 }
 
