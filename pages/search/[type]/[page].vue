c1a8cb93e2c851239166ba87e54b4ad9a07b7758
webyang-male <mr.yangweb@gmail.com>
2022-07-30 10:11:02 +0800

    feat(tab):Tab click jump event

--- pages/search/[type]/[page].vue
+++ pages/search/[type]/[page].vue
@@ -3,11 +3,9 @@
     <div>
         <h4 class="text-lg my-3"><b class="text-orange-400">{{ title }}</b>的搜索结果:</h4>
         <UiTab>
-            <UiTabItem active>
-                课程
-            </UiTabItem>
-            <UiTabItem>
-                专栏
+            <UiTabItem :active="type === item.value" v-for="(item, index) in tabs" :key="index"
+                @click="handleClick(item.value)">
+                {{ item.lable }}
             </UiTabItem>
         </UiTab>
     </div>
@@ -21,6 +19,28 @@ definePageMeta({
 //动态标题-搜索
 const route = useRoute();
 const title = ref(route.query.keyword || "");
+const type = ref(route.params.type)
 useHead({ title });
+
+let tabs = [{
+    lable: "课程",
+    value: "course"
+}, {
+    lable: "专栏",
+    value: "column"
+}];
+
+//点击跳转事件
+let handleClick = (t) => {
+    navigateTo({
+        params: {
+            ...route.params,
+            type: t
+        },
+        query: {
+            ...route.query
+        }
+    })
+}
 </script>
 
