a2c9c91c05b193a8a4239934ba122099336f794f
webyang-male <mr.yangweb@gmail.com>
2022-08-28 12:43:12 +0800

    feat:Coupon record page development completed

--- components/User/CouponList.vue
+++ components/User/CouponList.vue
@@ -1,11 +1,57 @@
 <!-- 我的优惠券组件 -->
 <template>
-    <n-card>
-        {{ item }}
+    <n-card class="coupon" :class="{ 'coupon-disabled': (btn != '立即使用') }">
+        <h4>CNY {{ item.price }}</h4>
+        <p>适用{{ o[item.type] }}：{{ item.title }}</p>
+        <p class="!text-xs mt-3">有效期: {{ item.start_time }} - {{ item.end_time }}</p>
+        <template #footer>
+            <div class="flex justify-end">
+                <n-button :type="btn != '立即使用' ? 'tertiary' : 'warning'" 
+                :disabled="btn != '立即使用'"
+                @click="open"
+                >
+                    {{ btn }}
+                </n-button>
+            </div>
+        </template>
     </n-card>
 </template>
 
 <script setup>
 import { NCard, NButton } from "naive-ui"
-defineProps({ item: Object })
+const props = defineProps({ item: Object })
+
+const o = {
+    course: "课程",
+    column: "专栏"
+}
+
+const btn = computed(() => {
+    const v = props.item
+    v.expires = (new Date(v.end_time)).getTime() < (new Date()).getTime()
+    return v.used ? "已使用" : (v.expires ? "已过期" : "立即使用")
+})
+
+const open = () => {
+    const {item} = props
+    navigateTo(`/createorder?id=${item.goods_id}&type=${item.type}`)
+}
 </script>
+
+<style>
+.coupon {
+    @apply mb-5 bg-orange-500 !border-0;
+}
+
+.coupon h4 {
+    @apply text-2xl mb-2 font-bold text-white;
+}
+
+.coupon p {
+    @apply text-sm text-light-300;
+}
+
+.coupon-disabled {
+    @apply  !bg-gray-300;
+}
+</style>
--- pages/user/coupon/[page].vue
+++ pages/user/coupon/[page].vue
@@ -4,7 +4,7 @@
         <div class="p-3">
             <!-- <UserBuyList v-for="(item, index) in rows" :key="index" :item="item"/> -->
             <n-grid :x-gap="20" :y-gap="5" :cols="2">
-                <n-grid-item v-for="(item, index) in rows.reverse()" :key="index">
+                <n-grid-item v-for="(item, index) in rows" :key="index">
                     <UserCouponList :item="item"/>
                 </n-grid-item>
             </n-grid>
