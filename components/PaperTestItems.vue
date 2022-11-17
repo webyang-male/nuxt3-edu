e7774cae26f9652ecaa0dfde1d3202ec797fd38a
webyang-male <mr.yangweb@gmail.com>
2022-11-17 21:39:42 +0800

    feat: Component development of blank filling and question answering

--- components/PaperTestItems.vue
+++ components/PaperTestItems.vue
@@ -1,27 +1,59 @@
-<!-- 考试题目列表组件 -->
+<!-- 考试题目列组件 -->
 <template>
     <div class="test-item">
         <div class="title">
-            第{{ index + 1 }}题 <span class="text-orange-400">{{ typeOptions[item.type] }}</span>
+            第{{ index + 1 }}题 {{ typeOptions[item.type] }}
         </div>
         <div class="p-4">
+            <!-- 题目内容 -->
             <p class="mb-5" v-html="item.title"></p>
+
             <!-- 题目选项 -->
             <div v-if="item.type == 'radio' || item.type == 'trueOrfalse' || item.type == 'checkbox'">
-                <p class="tips">答案:</p>
+                <p class="tip">答案：</p>
                 <ul>
-                    <li class="options" v-for="(q, qI) in item.options" :key="qI" :class="{ active: isActive(qI) }"
-                        @click="handleChooseOption(qI)">
-                        <span class="mr-2">{{ en[Number(qI + 1)] }}</span>
+                    <li class="option" v-for="(q, qI) in item.options" :key="qI" @click="handleChooseOption(qI)"
+                        :class="{ active: isActive(qI) }">
+                        <span class="mr-2">{{ en[qI] }}</span>
                         <div v-html="q"></div>
                     </li>
                 </ul>
             </div>
+
+            <!-- 填空 -->
+            <div v-else-if="item.type == 'completion'">
+                <div class="mb-3 border rounded p-3" v-for="(uv, uvI) in modelValue" :key="uvI">
+                    <div class="flex items-center justify-between mb-3">
+                        <h5 class="text-sm">填空 {{ uvI + 1 }}</h5>
+                        <n-icon v-if="uvI > 0" @click="remove(uvI)">
+                            <Close />
+                        </n-icon>
+                    </div>
+                    <n-input v-model:value="modelValue[uvI]" type="textarea" :placeholder="'填空 ' + (uvI + 1)" />
+                </div>
+
+                <n-button type="primary" dashed class="w-full my-3" @click="add">
+                    添加填空
+                </n-button>
+
+            </div>
+
+            <!-- 问答 -->
+            <div v-else>
+                <div class="mb-3 border rounded p-3">
+                    <n-input v-model:value="modelValue[0]" type="textarea" placeholder="请填写内容" clearable />
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script setup>
+import { NIcon, NInput, NButton, createDiscreteApi } from 'naive-ui';
+
+import { Close } from "@vicons/ionicons5"
+
 const props = defineProps({
     item: Object,
     index: Number
@@ -80,27 +112,47 @@ watch(modelValue, (newval) => {
     //针对数组
     deep: true
 })
+
+
+//移除填空
+function remove(i) {
+    const { dialog } = createDiscreteApi(["dialog"])
+    dialog.warning({
+        content: "是否要删除该填空？",
+        positiveText: "确定",
+        negativeText: "取消",
+        onPositiveClick: () => {
+            modelValue.value.splice(i, 1)
+        },
+    });
+}
+
+//添加填空
+function add() {
+    modelValue.value.push("")
+}
+
 </script>
 
 <style>
 .test-item {
-    @apply shadow mb-5 rounded;
+    @apply shadow rounded mb-5;
 }
 
 .test-item .title {
-    @apply p-4 bg-gray-100 border-b text-sm
+    @apply bg-gray-100 p-4 border-b text-sm;
 }
 
 .test-item .tip {
-    @apply text-xs mb-3 text-gray-500
+    @apply text-xs mb-3 text-gray-500;
 }
 
-.test-item .options {
-    transition: all .2s;
-    @apply flex border rounded p-2 mb-2 cursor-pointer text-sm hover: ( !border-blue-500 !bg-light-blue-50 !text-light-blue-600) active:( !bg-light-blue-100 !text-black)
+.test-item .option {
+    transition: all 0.2s;
+    @apply flex border p-2 rounded text-sm mb-2 cursor-pointer hover: ( !border-light-blue-500 !bg-light-blue-50 text-light-blue-600) active:( !bg-light-blue-200);
 }
 
 .test-item .active {
-    @apply !border-blue-500 !bg-light-blue-50 !text-light-blue-600
+    @apply !border-light-blue-500 !bg-light-blue-50 text-light-blue-600;
 }
 </style>
