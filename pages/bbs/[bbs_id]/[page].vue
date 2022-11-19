826c676e0bd61d7bdf274c82f240c24000738abd
webyang-male <mr.yangweb@gmail.com>
2022-11-19 16:03:19 +0800

    test(v0.5): Community page layout

--- pages/bbs/[bbs_id]/[page].vue
+++ pages/bbs/[bbs_id]/[page].vue
@@ -1,14 +1,34 @@
 <template>
     <div>
-        <BbsTabs/>
-        问答社区 {{ $route.params.bbs_id }} ~ {{ $route.params.page }} ~ {{ $route.name }}
+        <BbsTabs />
+        <n-grid :x-gap="20" :y-gap="5">
+            <n-grid-item :span="18">
+
+            </n-grid-item>
+            <n-grid-item :span="6">
+                <n-button type="primary" class="w-full mb-5" @click="">发布帖子</n-button>
+                <HotCourseList />
+            </n-grid-item>
+        </n-grid>
+
     </div>
 </template>
 <script setup>
+import { NGrid, NGridItem, NButton } from 'naive-ui';
 const route = useRoute()
 // console.log(route.params.bbs_id,route.params.page)
 
-useHead({
-    title:"社区问答页"
+
+definePageMeta({
+    middleware(to, from) {
+        const { bbs_id, page } = to.params
+        if (!isNaN(+bbs_id) && !isNaN(+page)) {
+            useHead({
+                title: "社区问答页"
+            })
+            return true
+        }
+        return abortNavigation("页面走丢了~")
+    }
 })
 </script>
