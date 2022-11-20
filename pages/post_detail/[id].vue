0f2bf8277e7f26172717dc5ee2382ff8172a2b90
webyang-male <mr.yangweb@gmail.com>
2022-11-20 14:38:56 +0800

    feat(Post Details Page):Complete Post content

--- pages/post_detail/[id].vue
+++ pages/post_detail/[id].vue
@@ -15,13 +15,56 @@
             <n-grid-item :span="18">
                 <LoadingGroup :pending="pending" :error="error">
                     <section class="bg-white mb-6 rounded">
-                        {{ data }}
+                        <div class="flex p-5 pb-0">
+                            <n-avatar :size="40" :src="data.user.avatar"></n-avatar>
+                            <div class="ml-2 flex-1">
+                                <h4 class="!text-orange-300">
+                                    {{ data.user.name }}
+                                    <n-tag class="ml-1" size="small" type="success" :bordered="false"
+                                        v-if="data.is_top">
+                                        置顶
+                                    </n-tag>
+                                </h4>
+                                <small class="text-gray-500">发布时间：{{ data.created_time }}</small>
+                            </div>
+                        </div>
+
+                        <NDivider />
+
+                        <!-- 帖子内容 -->
+                        <section class="p-5 pt-0">
+                            <div v-for="(item, index) in data.content" :key="index">
+                                <p class="postText">{{ item.text }}</p>
+                                <div class="flex flex-col items-center">
+                                    <n-image-group>
+                                        <n-space>
+                                            <n-image class="mb-2 rounded shadow" v-for="(img, imgi) in item.images"
+                                                :key="imgi" :src="img" object-fit="cover" />
+                                        </n-space>
+                                    </n-image-group>
+                                </div>
+                            </div>
+
+                            <div class="flex justify-center items-center">
+                                <n-button secondary strong size="tiny" class="mr-3"
+                                    :type="data.issupport ? 'primary' : 'tertiary'" @click.stop="handleSupport(data)"
+                                    :loading="supportLoading">
+                                    <template #icon>
+                                        <n-icon>
+                                            <ThumbsUpSharp />
+                                        </n-icon>
+                                    </template>
+                                    点赞 {{ data.support_count || 0 }}
+                                </n-button>
+                            </div>
+                        </section>
+
                     </section>
                 </LoadingGroup>
 
                 <!-- 评论区 -->
-                <NDivider/>
-                
+                <NDivider />
+
             </n-grid-item>
             <n-grid-item :span="6">
                 <n-button type="primary" class="mb-5 w-full">发布帖子</n-button>
@@ -32,7 +75,8 @@
 </template>
 
 <script setup>
-import { NButton, NBreadcrumb, NBreadcrumbItem, NGrid, NGridItem, NDivider } from 'naive-ui';
+import { NIcon, NImage, NTag, NButton, NBreadcrumb, NBreadcrumbItem, NGrid, NGridItem, NDivider, NAvatar } from 'naive-ui';
+import { ThumbsUpSharp } from "@vicons/ionicons5"
 
 const route = useRoute();
 const id = route.params.id
@@ -55,10 +99,21 @@ const title = computed(() => {
 
 useHead({ title })
 
+//点赞点击事件
+const {
+    supportLoading,
+    handleSupport
+} = useHandleSupportPost()
+
+
 //用户权限验证
 definePageMeta({
     middleware: ["auth"]
 })
 
 </script>
-
+<style>
+.postText {
+    text-indent: 2em !important;
+}
+</style>
